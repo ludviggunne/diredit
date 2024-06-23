31571e3d15f72666e7932566c7c49fb84d21c1e4
ludviggl <ludvig.gunne.lindstrom.dev@proton.me>
2024-06-23 17:33:37 +0200

    use flake

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, ... }:
+    inputs.flake-utils.lib.eachDefaultSystem (
+    system:
+      let
+        pkgs = import inputs.nixpkgs {inherit system;};
+      in
+      rec {
+        devShells = rec {
+          default = develop;
+          develop = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              autoreconfHook
+              gdb
+              indent
+              clang-tools
+            ];
+          };
+          use = pkgs.mkShell {
+            nativeBuildInputs = [ packages.diredit ];
+          };
+        };
+
+        packages = rec {
+            default = diredit;
+            diredit = pkgs.stdenv.mkDerivation {
+                pname = "diredit";
+                version = "1.0";
+                src = ./.;
+                nativeBuildInputs = with pkgs; [ autoreconfHook ];
+              	preConfigure = ''
+              		autoreconf --install
+              	'';
+              };
+          };
+      }
+    );
+}
